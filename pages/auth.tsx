e8ba2c04f37b6e0a09c7c9ea3fdfaf77d3f7ade7
Lucas Rocha <santoslucas.dev@gmail.com>
2023-04-11 21:50:27 -0300

    WIP: Settingup google and github OAuth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -4,6 +4,8 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import { signIn } from "next-auth/react";
 import Input from "@/components/Input/Input";
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
 const Auth = () => {
   const router = useRouter();
@@ -33,7 +35,7 @@ const Auth = () => {
     } catch (error) {
       console.log(error);
     }
-  }, [email, name, password]);
+  }, [email, password, router]);
 
   const register = useCallback(async () => {
     try {
@@ -90,7 +92,21 @@ const Auth = () => {
             >
               {variant === "login" ? "Login" : "Sign up"}
             </button>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div
+                onClick={() => signIn("google", { callbackUrl: "/" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FcGoogle size={30} />
+              </div>
 
+              <div
+                onClick={() => signIn("github", { callbackUrl: "/" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FaGithub size={30} />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-12">
               {variant === "login"
                 ? "First time using Netflix ?"
